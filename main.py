56529e09073ed6f07a0588b6c8b9b151fd7613e3
Akshat Khatri <jitikhatri@gmail.com>
2024-08-02 19:24:13 +0530

    first changes

---
+++ main.py
@@ -0,0 +1,20 @@
+import torch
+
+
+# This is a sample Python script.
+
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, name')  # Press F9 to toggle the breakpoint.
+    print(torch.__version__)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
